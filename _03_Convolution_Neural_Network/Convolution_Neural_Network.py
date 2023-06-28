c8b25ed29e5293586347fd6f40082f4527463a58
unknown <208644219@qq.com>
2023-06-28 23:48:16 +0800

    diwuci

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -73,8 +73,6 @@ class NeuralNetwork(nn.Module):      #Alexnet
 
 
  def read_data():
-     # 这里可自行修改数据预处理，batch大小也可自行调整
-     # 保持本地训练的数据读取和这里一致
      dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor())
      dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False, transform=torchvision.transforms.ToTensor())
      data_loader_train = DataLoader(dataset=dataset_train, batch_size=256, shuffle=True)
